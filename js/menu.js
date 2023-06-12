92fd7ce1b3abe9af32684acf3de691a52ded5e6d
Pokecrafter <pokecrafterjanek123@gmail.com>
2023-06-12 19:28:04 +0200

    input dziala ale wypierdala wszystko inne

--- js/menu.js
+++ js/menu.js
@@ -4,6 +4,9 @@ const pBazy = document.getElementById("pBazy");
 const pSS = document.getElementById("pSS");
 const pNudy = document.getElementById("pNudy");
 
+const inputBazy = document.getElementById("inputBazy");
+const inputSS = document.getElementById("inputSS");
+const inputNudy = document.getElementById("inputNudy");
 
 class person {
     constructor(name, baza, ss, nuda, opinie) {
@@ -29,7 +32,7 @@ fetch('data/opinions.json')
         var i = 1;
 
         input.forEach(element => {
-            people.push(new person(element.Name, element.Baza, element.SS, element.Nuda, element.Opinie));
+            people.push(new person(element.name, element.baza, element.ss, element.nuda, element.opinie));
             const newOption = document.createElement("option");
             newOption.text = element.Name;
             newOption.className = "opcja";
@@ -38,8 +41,8 @@ fetch('data/opinions.json')
             i++;
         });
 
-
         console.log(people);
+            
     })
     .catch(error => {
         console.log('Error:', error);
@@ -51,3 +54,35 @@ target.addEventListener('change', function() {
     pSS.innerText = people[selectedValue].ss * 100 + "%";
     pNudy.innerText = people[selectedValue].nuda * 100 + "%";
 });
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const selectedValue = target.value - 1;
+    const ilOpini = people[selectedValue].opinie;
+
+    people[selectedValue].baza = ((people[selectedValue].baza * ilOpini) + inputBazy) / (ilOpini + 1);
+    people[selectedValue].ss = ((people[selectedValue].ss * ilOpini) + inputSS) / (ilOpini + 1);
+    people[selectedValue].nuda = ((people[selectedValue].nuda * ilOpini) + inputNudy) / (ilOpini + 1);
+
+    people[selectedValue].opinie = people[selectedValue].opinie + 1;
+
+    updatedData = JSON.stringify(people);
+    console.log(updatedData)
+
+    fetch('data/opinions.json', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: updatedData
+    })
+    .then(response => response.json())
+    .then(newData => {
+        console.log('JSON file updated successfully:', newData);
+    })
+    .catch(error => {
+        console.log('Error:', error);
+    });
+    
+}
